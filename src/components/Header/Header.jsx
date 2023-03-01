f8b9800bdb69f45dbacd50b0c58fce6112664676
Manjot Virdi <virdi_manjot@hotmail.com>
2023-02-28 16:04:27 -0800

    Move NavContainer and Button components to Header; make button styling a global style in App.scss; add default font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import "./Header.scss";
 import logo from "../../assets/logo/BrainFlix-logo.svg";
-import NavContainer from "../NavContainer/NavContainer";
 
 function Header() {
     return (
@@ -9,7 +8,19 @@ function Header() {
                 <a className="nav__logo" href="#">
                     <img src={logo} alt="brainflix logo" />
                 </a>
-                <NavContainer />
+                <div className="nav__container">
+                    <div className="nav__search">
+                        {/* <img src= alt="search icon" className="nav__search-icon" /> */}
+                        <input type="text" name="search" id="search" placeholder="Search" />
+                    </div>
+                    <a className="button button__upload button__upload--tablet" href="#">
+                        Upload
+                    </a>
+                    <div className="nav__avatar"></div>
+                    <a className="button button__upload button__upload--mobile" href="#">
+                        Upload
+                    </a>
+                </div>
             </nav>
         </header>
     );
