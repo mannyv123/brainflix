7086c9e4e58a91494d3dea2675132dce848fbec9
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-03 14:35:02 -0800

    Format comment form avatar and content; fix white space under next video images

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,12 +1,14 @@
-import "./CommentCard";
+import "./CommentCard.scss";
 
 function CommentCard(props) {
+    const formattedDate = new Date(props.timestamp).toLocaleDateString();
     return (
         <li className="comment">
-            <img src="" alt="" className="comment__avatar" />
+            <div className="comment__avatar"></div>
+            {/* <img src="" alt="" className="comment__avatar" /> */}
             <div className="comment__content-container">
                 <p className="comment__name">{props.name}</p>
-                <p className="comment__timestamp">{props.timestamp}</p>
+                <p className="comment__timestamp">{formattedDate}</p>
                 <p className="comment__text">{props.comment}</p>
             </div>
         </li>
