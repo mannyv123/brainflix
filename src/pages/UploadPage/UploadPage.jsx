a3d18c26bf0a843adeddf924db17a5770e5dfe58
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-06 16:24:55 -0800

    add upload preview image; add styling to upload page; add form element to upload page

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,4 +1,5 @@
 import "./UploadPage.scss";
+import uploadPreview from "../../assets/images/Upload-video-preview.jpg";
 
 function UploadPage() {
     return (
@@ -6,7 +7,10 @@ function UploadPage() {
             <section className="upload">
                 <h1 className="upload__page-title">Upload Video</h1>
                 {/* no action in form as per sprint-2 requirements, functionality of the form not required */}
-                <form action=""></form>
+                <form action="" className="upload__form">
+                    <h3 className="upload__thumbnail-label">Video Thumbnail</h3>
+                    <img className="upload__thumbnail-img" src={uploadPreview} alt="upload preview image" />
+                </form>
             </section>
         </main>
     );
