dfca27ab7e0f578e9f665a92c320c184cf70f29d
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-06 16:48:53 -0800

    Add inputs to form and related labels; wrap form items into containers

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -8,8 +8,29 @@ function UploadPage() {
                 <h1 className="upload__page-title">Upload Video</h1>
                 {/* no action in form as per sprint-2 requirements, functionality of the form not required */}
                 <form action="" className="upload__form">
-                    <h3 className="upload__thumbnail-label">Video Thumbnail</h3>
-                    <img className="upload__thumbnail-img" src={uploadPreview} alt="upload preview image" />
+                    <div className="upload__thumbnail-container">
+                        <h3 className="upload__thumbnail-label">Video Thumbnail</h3>
+                        <img
+                            className="upload__thumbnail-img"
+                            src={uploadPreview}
+                            alt="upload preview image"
+                        />
+                    </div>
+                    <div className="upload__inputs-container">
+                        <label htmlFor="videoTitle">Title Your Video</label>
+                        <input
+                            type="text"
+                            name="videoTitle"
+                            id="videoTitle"
+                            placeholder="Add a title to your video"
+                        />
+                        <label htmlFor="videoDesc">Add a Video Description</label>
+                        <textarea
+                            name="videoDesc"
+                            id="videoDesc"
+                            placeholder="Add a description to your video"
+                        ></textarea>
+                    </div>
                 </form>
             </section>
         </main>
