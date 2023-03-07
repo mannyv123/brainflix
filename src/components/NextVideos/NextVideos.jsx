e8808017197e48c409f4c73a873fc3342809d37b
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-07 14:30:58 -0800

    Add axios get call to render video list; Comment out other components while testing

--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -1,19 +1,17 @@
-import { useState } from "react";
-import videoList from "../../data/videos.json";
+// import { useState } from "react";
+// import videoList from "../../data/videos.json";
 import "./NextVideos.scss";
 import NextVideoCard from "../NextVideoCard/NextVideoCard";
 
 /* Next Video Section; renders next videos using NextVideoCard component, 
 while excluding currently selected/playing video */
 function NextVideos(props) {
-    const [videos] = useState(videoList);
-
     return (
         <section className="next-video">
             <h3 className="next-video__header">Next Videos</h3>
             <ul className="next-video__list">
-                {videos
-                    .filter((video) => video.id !== props.currentVideo.id)
+                {props.videos
+                    // .filter((video) => video.id !== props.currentVideo.id)
                     .map((video) => {
                         return (
                             <NextVideoCard
--- src/pages/VideoPage/VideoPage.jsx
+++ src/pages/VideoPage/VideoPage.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
-import videoDetails from "../../data/video-details.json";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+// import videoDetails from "../../data/video-details.json";
 import Comments from "../../components/Comments/Comments";
 import CurrentVideo from "../../components/CurrentVideo/CurrentVideo";
 import CurrentVideoDetails from "../../components/CurrentVideoDetails/CurrentVideoDetails";
@@ -7,26 +9,45 @@ import NextVideos from "../../components/NextVideos/NextVideos";
 import "./VideoPage.scss";
 
 const apiUrl = "https://project-2-api.herokuapp.com";
+const apiKey = "d28fb146-1574-4fbd-a40b-9046fc985897";
 
 function VideoPage() {
-    const [videos] = useState(videoDetails);
-    const [currentVideo, setCurrentVideo] = useState(videoDetails[0]);
+    const [videos, setVideos] = useState([]);
+    const [currentVideo, setCurrentVideo] = useState({});
+    const { videoId } = useParams();
 
-    const handleVideoClick = (id) => {
-        const clickedVideo = videos.find((video) => video.id === id);
-        setCurrentVideo(clickedVideo);
-    };
+    // const handleVideoClick = (id) => {
+    //     const clickedVideo = videos.find((video) => video.id === id);
+    //     setCurrentVideo(clickedVideo);
+    // };
+
+    useEffect(() => {
+        getVideoList();
+    }, []);
+
+    function getVideoList() {
+        axios
+            .get(`${apiUrl}/videos?api_key=${apiKey}`)
+            .then((response) => {
+                setVideos(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 
     return (
         <main>
             {/* Note: videoSource just a placeholder; will not work until api added */}
-            <CurrentVideo currentVideo={currentVideo} />
+            {/* <CurrentVideo currentVideo={currentVideo} /> */}
             <section className="content">
                 <section className="content__current-video">
-                    <CurrentVideoDetails currentVideo={currentVideo} />
-                    <Comments currentVideo={currentVideo} />
+                    {/* <CurrentVideoDetails currentVideo={currentVideo} /> */}
+                    {/* <Comments currentVideo={currentVideo} /> */}
                 </section>
-                <NextVideos currentVideo={currentVideo} handleVideoClick={handleVideoClick} />
+                {/* <NextVideos videos={videos} currentVideo={currentVideo} handleVideoClick={handleVideoClick} /> */}
+                <NextVideos videos={videos} />
+                {console.log(videos)}
             </section>
         </main>
     );
