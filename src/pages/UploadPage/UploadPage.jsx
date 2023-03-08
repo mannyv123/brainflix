61a62a86f87902547349bab7969bebe37b3dde75
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-07 22:17:31 -0800

    Add useEffect to navigate to home after a delay when publish button clicked

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,44 +1,52 @@
-import { Navigate, Link } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
 import "./UploadPage.scss";
 import uploadPreview from "../../assets/images/Upload-video-preview.jpg";
 import publishIcon from "../../assets/icons/publish.svg";
 
 function UploadPage() {
-    const [upload, setUpload] = useState(
-        <button type="" className="button button__publish">
-            <img src={publishIcon} alt="publish icon" className="button__publish-icon" />
-            Publish
-            <div className="button__publish-icon button--hidden"></div>
-        </button>
-    );
+    const [upload, setUpload] = useState(false);
+    const timerId = useRef(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (upload) {
+            timerId.current = setTimeout(() => {
+                // setUpload(false); //this i need to change to navigate
+                navigate("/");
+            }, 5000);
+        }
+        console.log(upload);
+        return () => {
+            clearTimeout(timerId.current);
+        };
+    }, [upload, navigate]);
 
     const handleUploadClick = () => {
         // setUpload(`Thank you for your upload. Redirecting you to the main page...`);
         // console.log(setTimeout(() => <Navigate to="/" />, 5000));
 
-        setUpload(() => {
-            // return (
-            //     <div>
-            //         <p>Redirecting</p>
-            //         {setTimeout(
-            //             () => (
-            //                 <Navigate to="/" />
-            //             ),
-            //             1000
-            //         )}
-            //     </div>
-            // );
-
-            return (
-                <div>
-                    {alert("Thank you for your upload!")}
-                    <Navigate to="/" />;
-                </div>
-            );
-        });
+        setUpload(true);
     };
+    // return (
+    //     <div>
+    //         <p>Redirecting</p>
+    //         {setTimeout(
+    //             () => (
+    //                 <Navigate to="/" />
+    //             ),
+    //             1000
+    //         )}
+    //     </div>
+    // );
 
+    // return (
+    //     <div>
+    //         {alert("Thank you for your upload!")}
+    //         <Navigate to="/" />;
+    //     </div>
+    // );
+    console.log("whats happening");
     return (
         <main>
             <section className="upload">
@@ -70,7 +78,18 @@ function UploadPage() {
                             placeholder="Add a description to your video"
                         ></textarea>
                     </div>
-                    <div onClick={handleUploadClick}>{upload}</div>
+                    <div onClick={handleUploadClick}>
+                        {!upload ? (
+                            <button type="" className="button button__publish">
+                                <img src={publishIcon} alt="publish icon" className="button__publish-icon" />
+                                Publish
+                                <div className="button__publish-icon button--hidden"></div>
+                            </button>
+                        ) : (
+                            "Loading"
+                        )}
+                        {console.log("whats happening")}
+                    </div>
                 </form>
                 <Link className="upload__cancel" to="/">
                     <p>Cancel</p>
