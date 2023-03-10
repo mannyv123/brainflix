3f1f82e6d8121925591f3c9d6a73c18cf11b640d
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-10 13:25:51 -0800

    add path for Navlink on tablet upload button in header ; style error msg for comment form at tablet breakpoint

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
                             className="nav__search-input"
                         />
                     </div>
-                    <NavLink className="button button__upload--tablet">
+                    <NavLink to="/upload" className="button button__upload--tablet">
                         <img src={uploadIcon} alt="upload video icon" className="button__upload-icon" />
                         Upload
                         <div className="button__upload-icon button--hidden"></div>
