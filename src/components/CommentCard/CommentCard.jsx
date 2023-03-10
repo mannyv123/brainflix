437038484e9c659b2bee953992b8fbc3b0babb1a
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-09 22:38:18 -0800

    Add delete icon for comments; add styling to delete icon including hover and active effects

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,5 +1,6 @@
 import "./CommentCard.scss";
 import timeAgo from "../../utils/timeAgo";
+import deleteIcon from "../../assets/icons/icon-delete.svg";
 
 /* Component to be used for each comment for currently selected/playing video */
 function CommentCard({ comment, handleCommentsDelete, videoId }) {
@@ -7,18 +8,18 @@ function CommentCard({ comment, handleCommentsDelete, videoId }) {
     return (
         <li className="comment">
             <div className="comment__avatar"></div>
-            {/* <img src="" alt="" className="comment__avatar" /> */}
             <div className="comment__content-container">
                 <p className="comment__name">{comment.name}</p>
                 <p className="comment__timestamp">{timeSincePost}</p>
                 <p className="comment__text">{comment.comment}</p>
-                <p
+                <img
+                    className="comment__delete"
+                    src={deleteIcon}
+                    alt="Delete"
                     onClick={() => {
                         handleCommentsDelete(videoId, comment.id);
                     }}
-                >
-                    DELETE
-                </p>
+                />
             </div>
         </li>
     );
