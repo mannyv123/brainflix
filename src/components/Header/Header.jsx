64728f740fe7ea0ff847c60f2e0108bf82fc8588
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-10 16:05:22 -0800

    Change use of NavLink in header to Link; add divider line below inputs on upload page form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,16 @@ import "./Header.scss";
 import logo from "../../assets/logo/BrainFlix-logo.svg";
 import searchIcon from "../../assets/icons/search.svg";
 import uploadIcon from "../../assets/icons/upload.svg";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <header className="nav-header">
             {/* Main Navigation */}
             <nav className="nav">
-                <NavLink to="/">
+                <Link to="/">
                     <img className="nav__logo" src={logo} alt="brainflix logo" />
-                </NavLink>
+                </Link>
                 <div className="nav__container">
                     <div className="nav__search">
                         <img src={searchIcon} alt="search icon" className="nav__search-icon" />
@@ -23,17 +23,17 @@ function Header() {
                             className="nav__search-input"
                         />
                     </div>
-                    <NavLink to="/upload" className="button button__upload--tablet">
+                    <Link to="/upload" className="button button__upload--tablet">
                         <img src={uploadIcon} alt="upload video icon" className="button__upload-icon" />
                         Upload
                         <div className="button__upload-icon button--hidden"></div>
-                    </NavLink>
+                    </Link>
                     <div className="nav__avatar"></div>
-                    <NavLink to="/upload" className="button button__upload--mobile">
+                    <Link to="/upload" className="button button__upload--mobile">
                         <img src={uploadIcon} alt="upload video icon" className="button__upload-icon" />
                         Upload
                         <div className="button__upload-icon button--hidden"></div>
-                    </NavLink>
+                    </Link>
                 </div>
             </nav>
         </header>
