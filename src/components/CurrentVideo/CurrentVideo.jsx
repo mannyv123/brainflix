cc351805d970e430cb4274e38e2b64f1a7fec255
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-19 19:08:12 -0700

    remove commented out code

--- src/components/CurrentVideo/CurrentVideo.jsx
+++ src/components/CurrentVideo/CurrentVideo.jsx
@@ -1,57 +1,17 @@
 import "./CurrentVideo.scss";
 import { API_URL } from "../../pages/VideoPage/VideoPage";
-// import { useRef } from "react";
 
 /* Main/Hero Video */
 function CurrentVideo({ currentVideo }) {
-    // const videoRef = useRef(null);
-
     return (
-        <figure className="current-video">
+        <section className="current-video">
             <video
                 className="current-video__player"
                 poster={`${API_URL}${currentVideo.image}`}
                 src={`${currentVideo.video}/?api_key=manjot`}
                 controls
             ></video>
-            {/* <ul id="video-controls" className="current-video__controls">
-                <li>
-                    <button id="play" type="button">
-                        
-                    </button>
-                </li>
-                <li>
-                    <button id="stop" type="button">
-                        Stop
-                    </button>
-                </li>
-                <li className="current-video__progress">
-                    <progress id="progress" value="0" min="0">
-                        <span id="progress-bar"></span>
-                    </progress>
-                </li>
-                <li>
-                    <button id="mute" type="button">
-                        Mute/Unmute
-                    </button>
-                </li>
-                <li>
-                    <button id="volinc" type="button">
-                        Vol+
-                    </button>
-                </li>
-                <li>
-                    <button id="voldec" type="button">
-                        Vol-
-                    </button>
-                </li>
-                <li>
-                    <button id="fs" type="button">
-                        Fullscreen
-                    </button>
-                </li>
-            </ul> */}
-        </figure>
+        </section>
     );
 }
 
